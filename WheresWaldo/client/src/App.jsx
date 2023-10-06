53db704e9224909bbf73a8779637ba6259c1af3d
Phlegm Mignon <dmah@ucdavis.edu>
2023-10-06 05:10:25 -0700

    WIP game and char states

--- WheresWaldo/client/src/App.jsx
+++ WheresWaldo/client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Timer from "./components/Timer";
 import Gameboard from "./components/Gameboard";
+import BoardSelect from "./components/BoardSelect";
 import BoardImages from "./BoardImages.jsx";
 
 export default function App() {
@@ -11,6 +12,62 @@ export default function App() {
 
   const [gameState, setGameState] = useState("start");
   const [gameImage, setGameImage] = useState(gameImages[0]);
+  const [found, setFound] = useState([false]);
+
+  const setInitialFound = () => {
+    let foundStatuses = [];
+    gameImage.characters.forEach(() => {
+      foundStatuses.push(false);
+    });
+    setFound(foundStatuses);
+  };
+
+  const setFoundStatus = async (charId, clickPosition) => {
+    if (charId < 0 || charId > found.length - 1) return false;
+    // const valid = db.validatePosition(gameImage.id, charId, clickPos);
+    if (!valid) {
+      shakeGamePanel();
+      return false;
+    } else {
+      let newFound = found.slice();
+      newFound[charId] = true;
+      setFound(newFound);
+      checkGameWon();
+      return true;
+    }
+  };
+
+  //May need to change timer later
+  const renderGameState = () => {
+    switch (gameState) {
+      case "start":
+        return (
+          <BoardSelect
+            images={gameImages}
+            selected={gameImage.id}
+            setGameImage={setGameImage}
+            setFound={setInitialFound}
+            setStartTime={setStartTime}
+          />
+        );
+    }
+  };
+
+  const toggleGameState = () => {
+    switch (gameState) {
+      case "start":
+        setGameState("inProgress");
+        break;
+      case "inProgress":
+        setGameState("end");
+        break;
+      case "end":
+        setGameState("start");
+    }
+  };
+
+  //Name for highscore prompt
+  const [name, setName] = useState("test");
 
   //Timer states
   const [ms, setMs] = useState("");
